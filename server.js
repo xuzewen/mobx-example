6d6469223bcd7a07d65ce650f8751bf755f6d596
xuzewen <xzw_78154@sina.com>
2016-10-31 14:46:18 +0800

    “init”

---
+++ server.js
@@ -0,0 +1,37 @@
+var WebpackDevServer = require('webpack-dev-server')
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+var port = '3000';
+var hosts = '0.0.0.0'
+
+for (var key in config['entry']) {
+    var arr = [];
+    arr.unshift(`webpack-dev-server/client?http://${hosts}:${port}/`,'webpack/hot/dev-server','babel-polyfill', config['entry'][key]);
+    config['entry'][key] = arr;
+}
+
+
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+    hot:true,
+    //热加载必须的 inline
+    inline:true,
+
+    quiet: false,
+    compress: false,
+    historyApiFallback: true,
+    stats: {
+        // Config for minimal console.log mess.
+        assets: true,
+        colors: true,
+        version: false,
+        hash: true,
+        timings: true,
+        chunks: false,
+        chunkModules: true
+    }
+});
+
+server.listen(port);
+console.log(`Open http://${hosts}:${port}/`);
